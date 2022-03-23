dc08669f84778b084a29a673fa4bd9942e1ce41d
suman <suman@SUMAN>
2022-03-23 19:32:43 +0530

    Add project files.

---
+++ BookStore.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="BookStore.aspx.cs" Inherits="AppBookStore.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Book Name: </td>
+                    <td><asp:TextBox  ID="txtName" runat ="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Book Price: </td>
+                    <td><asp:TextBox  ID="txtPrice" runat ="server"></asp:TextBox></td>
+                </tr>
+                <tr>
+                    <td>Author Name: </td>
+                    <td><asp:TextBox  ID="txtAuthorName" runat ="server"></asp:TextBox></td>
+                </tr>
+                
+                 <tr>
+                    <td> </td>
+                    <td><asp:Button id="BtnSave" runat="server" Text="SAVE" OnClick="BtnSave_Click"
+                        /> </td>
+                </tr>
+
+                  <tr>
+                    <td> </td>
+                    <td><asp:GridView id="grd" runat="server" ></asp:GridView> </td>
+                </tr>
+
+
+
+
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ BookStore.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace AppBookStore
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("data source=SUMAN\\SQLEXPRESS; initial catalog=Bookstore; integrated security=true"); 
+        protected void Page_Load(object sender, EventArgs e) 
+        {
+            Display();
+        }
+
+       
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into Library(bookname, price, author) values('" +txtName.Text+ "' ,'" +txtPrice.Text + "', '" + txtAuthorName.Text + "')", con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Display();
+            MessageBox.Show("Record Saved Successfully", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        void Display()
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("select * from Library", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            con.Close();
+
+            grd.DataSource = dt;
+            grd.DataBind();
+        }
+    }
+}
